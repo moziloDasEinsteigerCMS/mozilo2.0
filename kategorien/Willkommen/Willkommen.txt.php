d69d51a78a30341c5ecf870702dbe7277bbecb0d
moziloRainer <bist.narrisch@arcor.de>
2012-11-15 08:46:35 +0100

    Update kategorien/Willkommen/Willkommen.txt.php

--- kategorien/Willkommen/Willkommen.txt.php
+++ kategorien/Willkommen/Willkommen.txt.php
@@ -1,11 +1,11 @@
 <?php die(); ?>
 [ueber1|Willkommen]
-...in Ihrem moziloCMS! Passen Sie alles an Ihre Bedürfnisse und Ziele an: Loggen Sie sich dafür in die moziloCMS-Konfigurationsoberfläche [html|<a href="admin/" target="_blank">moziloAdmin</a>] ein. Der initiale Benutzer heißt "admin" und hat das Paßwort "install" - bitte vergessen Sie nicht, als erstes diese Zugangsdaten zu ändern!
+Herzlichen Glückwunsch du hast mit moziloCMS nun eines der am einfachsten zu bedienenden CMS-Systeme auf deinem Webspace installiert. Wir wünschen dir noch viel Spaß und natürlich viel Erfolg mit deiner Internetseite, die du mit moziloCMS 2.0 ab heute leicht und übersichtlich gestalten wirst.
 
-Wie alles hier im moziloCMS funktioniert, ist auf [link|http://cms.mozilo.de] beschrieben; für Fragen und Anregungen steht Ihnen das [link=mozilo-Supportforum|http://forum.mozilo.de/] zur Verfügung.
+Wie alles hier im moziloCMS 2.0 funktioniert, ist auf [link|http://cms.mozilo.de] beschrieben; für Fragen und Anregungen steht Dir das [link=mozilo-Supportforum|http://forum.mozilo.de/] zur Verfügung.
 
-[fett|Viel Spaß mit moziloCMS!]
-Ihr [link=mozilo-Team|http://www.mozilo.de]
+[fett|Und noch einmal: viel Spaß mit moziloCMS!]
+Dein [link=mozilo-Team|http://www.mozilo.de]
 
 [----]
 Hier noch als kleine Demonstration ein Bild, eine Datei und der Link zu einer Galerie:
