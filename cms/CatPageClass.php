286ae3fe94b705dc38dc9477fef0b2cb09b2af81
stefanbe <mozilo@stefanbe.de>
2013-01-30 15:04:56 +0100

    -html_amp~ replace mal Provisorisch hier eingebaut

--- cms/CatPageClass.php
+++ cms/CatPageClass.php
@@ -354,6 +354,7 @@ class CatPageClass {
     # $file = optinal und muss true sein wenn page eine datei ist
     # bei nur page/file wird wenn vorhanden CAT_REQUEST genommen
     function split_CatPage_fromSyntax($syntax_catpage, $file = false) {
+        $syntax_catpage = str_replace('-html_amp~','&',$syntax_catpage);
         $syntax_catpage = replaceFileMarker($syntax_catpage,false);
         $syntax_catpage = $this->get_AsKeyName($syntax_catpage, true);
         $syntax_catpage = str_replace(":","%3A",$syntax_catpage);
