5351e8cc61fdfdd75858e38f56dc4918a5623a17
stefanbe <mozilo@stefanbe.de>
2014-03-23 14:49:39 +0100

    Links für Admin und Doku. gefixt

--- kategorien/Willkommen/Willkommen.txt.php
+++ kategorien/Willkommen/Willkommen.txt.php
@@ -2,9 +2,9 @@
 [ueber1|Willkommen...]
 ... im frisch installierten moziloCMS 2.0!
 
-Die Zugangsdaten für die [html|<a href="../admin/" target="_blank">moziloCMS-Konfigurationsoberfläche (moziloAdmin)</a>] wurden bei der Installation bereits angegeben. Im moziloAdmin können nun weitere Einstellungen für das moziloCMS getätigt werden.
+Die Zugangsdaten für die [html|<a href="admin/" target="_blank">moziloCMS-Konfigurationsoberfläche (moziloAdmin)</a>] wurden bei der Installation bereits angegeben. Im moziloAdmin können nun weitere Einstellungen für das moziloCMS getätigt werden.
 
-Wie alles hier im moziloCMS funktioniert, ist in der zum [html|<a href="../docu/" target="_blank">moziloCMS gehörenden Docu</a>] sowie auf [link|http://www.mozilo.de] beschrieben; für Fragen und Anregungen steht das [link=mozilo-Supportforum|http://www.mozilo.de/forum] zur Verfügung.
+Wie alles hier im moziloCMS funktioniert, ist in der zum [html|<a href="docu/" target="_blank">moziloCMS gehörenden Docu</a>] sowie auf [link|http://www.mozilo.de] beschrieben; für Fragen und Anregungen steht das [link=mozilo-Supportforum|http://www.mozilo.de/forum] zur Verfügung.
 
 Das frisch installierte moziloCMS 2.0 liefert zu Demozwecken eine bereits angelegte Kategorie "Willkommen" sowie die dazugehörenden Inhaltsseiten "Willkommen", "Kontakt" und "Eine Galerie" mit.
 Bitte beachten: die auf den Seiten "Kontakt" und "Eine Galerie" genutzten Plugins (CONTACT bzw. Galerie) müssen im moziloAdmin zunächst aktiviert werden, um korrekt dargestellt werden zu können.
