da461ac38fb18da898933b30b041104ae0b1fb0a
stefanbe <mozilo@stefanbe.de>
2014-09-01 14:52:31 +0200

    Revision

--- cms/DefaultConfCMS.php
+++ cms/DefaultConfCMS.php
@@ -2,7 +2,7 @@
 
 define("CMSVERSION","2.0");
 define("CMSNAME","Amalia");
-define("CMSREVISION","18");
+define("CMSREVISION","19");
 
 #!!!!!!!! die version müssen wir noch checken
 define("MIN_PHP_VERSION","5.1.2");
